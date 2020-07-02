9c21345922ae88fa7aa3a1871fcb55dd2684adb6
denis2020usp <deniskeler@usp.br>
2020-07-02 18:30:31 +0000

    m005 feito

--- m005.c
+++ m005.c
@@ -27,8 +27,25 @@
 
 int determinant (int m[3][3])
 {
-  int i;
-  return 0;
+  int resultado;
+  int dp;
+  int ds;
+ dp =
+  ( m[0] [0] * m[1] [1] * m[2] [2] 
+  +
+  m [1] [0] * m[2] [1] * m[0] [2]
+  +
+  m[2] [0]* m[0] [1] * m[1] [2]);
+
+  ds =
+  (-1* (m[0] [2] * m[1] [1]* m[2] [0])
+  +
+  -1*(m[1] [2] * m[2] [1] *m[0] [0])
+  +
+  -1*(m[2] [2]* m[0] [1] * m[1] [0]));
+ 
+  resultado = dp + ds;
+  return resultado;
 }
 
 
